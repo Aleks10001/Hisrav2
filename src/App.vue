58da267c8e29544faecfddbc218f771e45a9c9df
Khisrav <kh.khisrav2018@gmail.com>
2024-04-13 19:47:37 +0300

    boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RoadmapComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RoadmapComponent from './components/RoadmapComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RoadmapComponent
   }
 }
 </script>
